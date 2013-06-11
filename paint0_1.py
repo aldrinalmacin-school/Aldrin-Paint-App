55678685175776cc76f4cd1078049de87e4bcedd
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-06-10 21:55:17 -0400

    Added check key pressed

--- paint0_1.py
+++ paint0_1.py
@@ -15,10 +15,57 @@ class Canvas(pygame.Surface):
   def clear(self):
     self.fill((255,255,255))
 
+class Pen:
+  BLACK = (0, 0, 0)
+  RED = (255, 0, 0)
+  GREEN = (0, 255, 0)
+  WHITE = (255, 255, 255)
+  BLUE = (0, 0, 255)
+  def __init__(self):
+    self.line_width = 1
+    self.draw_color = Pen.BLACK
+
+  def check_key_pressed(self, key_pressed):
+    # Draw Colors
+    if key_pressed == pygame.K_r:
+      self.draw_color = Pen.RED
+    elif key_pressed == pygame.K_g:
+      self.draw_color = Pen.GREEN
+    elif key_pressed == pygame.K_w:
+      self.draw_color = Pen.WHITE
+    elif key_pressed == pygame.K_b:
+      self.draw_color = Pen.BLUE
+    elif key_pressed == pygame.K_k:
+      self.draw_color = Pen.BLACK
+
+    #line widths
+    elif key_pressed == pygame.K_1:
+      self.line_width = 1
+    elif key_pressed == pygame.K_2:
+      self.line_width = 2
+    elif key_pressed == pygame.K_3:
+      self.line_width = 3
+    elif key_pressed == pygame.K_4:
+      self.line_width = 4
+    elif key_pressed == pygame.K_5:
+      self.line_width = 5
+    elif key_pressed == pygame.K_6:
+      self.line_width = 6
+    elif key_pressed == pygame.K_7:
+      self.line_width = 7
+    elif key_pressed == pygame.K_8:
+      self.line_width = 8
+    elif key_pressed == pygame.K_9:
+      self.line_width = 9
+
+    print "Line width: ", self.line_width
+    print "Color: ", self.draw_color
+
 def main():
   pygame.init()
   canvas = Canvas(MAIN_WIDTH, MAIN_HEIGHT - STRIP_HEIGHT)
   screen = pygame.display.set_mode(MAIN_SIZE)
+  pen = Pen()
   pygame.display.set_caption(MAIN_TITLE)
 
   frame_clock = pygame.time.Clock()
@@ -27,8 +74,26 @@ def main():
     frame_clock.tick(30)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_running = False
+      if event.type == pygame.QUIT:
+        game_running = False
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_q:
+          print "Quit the game"
+          game_running = False
+        elif event.key == pygame.K_c:
+          print "Clear the canvas"
+          #clear screen
+          canvas.clear()
+        elif event.key == pygame.K_s:
+          print "Save the Drawing"
+          #save picture
+          pygame.image.save(canvas, "painting.bmp")
+        elif event.key == pygame.K_l:
+          print "Load the Drawing"
+          #load picture
+          canvas = pygame.image.load("painting.bmp")
+        else:
+          pen.check_key_pressed(event.key)
 
     screen.blit(canvas, (0, STRIP_HEIGHT))
     pygame.display.flip()
